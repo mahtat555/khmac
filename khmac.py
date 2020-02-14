9500572365a6f8ad475f97753f35fedf711d3ab1
mahtat555 <yassinmhtat@gmail.com>
2020-02-14 16:59:33 +0100

    update  the  file

--- khmac.py
+++ khmac.py
@@ -8,13 +8,12 @@ standard <<Key hash message authentication code (HMAC) (FIPS PUB 198).>>
 
 """
 
-from _operator import _compare_digest as cmp
 import hashlib
-
+from _operator import _compare_digest as cmp
 
 # ipad and opad were chosen in order to have an important Hamming distance
-opad = bytes(i ^ 0x5c for i in range(256))
-ipad = bytes(i ^ 0x36 for i in range(256))
+OPAD = bytes(i ^ 0x5c for i in range(256))
+IPAD = bytes(i ^ 0x36 for i in range(256))
 
 ## Exclusive-Or function
 # For Exclusive-Or `Key` with `ipad` and `Key` with `opad`
@@ -22,7 +21,7 @@ XOR = bytes.translate
 
 
 # KHMAC : keyed-hash message authentication code.
-class KHMAC :
+class KHMAC:
     """KHMAC(key :bytes, msg :bytes, hash="sha1").
 
     Allows the generation of a MAC (Message Authentication Code)
@@ -61,7 +60,7 @@ class KHMAC :
 
     """
 
-    def __init__(self, key :bytes, msg :bytes, hash = "sha1"):
+    def __init__(self, key: bytes, msg: bytes, hash_func="sha1"):
         """Create a new `KHMAC` object.
 
         Parameters :
@@ -75,32 +74,32 @@ class KHMAC :
                  'sha256', 'sha1'}
 
         """
-        if hasattr(hashlib, hash):
+        if hasattr(hashlib, hash_func):
             self.hash = hash
-            hash = getattr(hashlib, hash)
-        else :
+            hash_func = getattr(hashlib, hash_func)
+        else:
             raise ValueError("unsupported hash type `{}`".format(hash))
 
-        block_size = hash().block_size
+        block_size = hash_func().block_size
         key_size = len(key)
 
         # step 1 -- determine key
-        if key_size > block_size :
+        if key_size > block_size:
             # hash `key`, then append
             # zeros to create a block_size-bytes string `key`
-            key = hash(key).ljust(block_size, b'\x00')
+            key = hash_func(key).ljust(block_size, b'\x00')
 
-        elif key_size < block_size :
+        elif key_size < block_size:
             # Append zeros to the end of `key`,
             # to create a block_size-bytes string `key`
             key = key.ljust(block_size, b'\x00')
 
         ## Calculate hmac
         # step 2 -- calculate hash(k + opad)
-        self.block_1 = hash(XOR(key, opad))
+        self.block_1 = hash_func(XOR(key, OPAD))
 
         # step 3 -- calculate hash((k + ipad) || m)
-        self.block_2 = hash(XOR(key, ipad))
+        self.block_2 = hash_func(XOR(key, IPAD))
         self.block_2.update(msg)
 
 
@@ -111,21 +110,21 @@ class KHMAC :
         return hmac
 
 
-    def digest(self) -> bytes :
+    def digest(self) -> bytes:
         """Return the digest value as a bytes object.
 
         """
         return self.__finalize().digest()
 
 
-    def hexdigest(self) -> str :
+    def hexdigest(self) -> str:
         """Return the digest value as a string of hexadecimal digits.
 
         """
         return self.__finalize().hexdigest()
 
 
-    def update(self, msg :bytes) -> None:
+    def update(self, msg: bytes) -> None:
         """Update the hmac object. Repeated calls are equivalent to a single
         call with the concatenation of all the arguments:
             >>> h.update(a)
@@ -137,7 +136,7 @@ class KHMAC :
         self.block_2.update(msg)
 
 
-    def verify(self, hmac :bytes) -> bool :
+    def verify(self, hmac: bytes) -> bool:
         """Checks if a `hmac` corresponds to the message using
         the secret key `key`.
 
