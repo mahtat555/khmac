853792b89d62276c4916b8f8e5837b741f0168c1
Mahtat <yassinmhtat@gmail.com>
2020-12-04 22:52:59 +0100

    Add project information to the __init__.py file

---
+++ khmac/__init__.py
@@ -0,0 +1,21 @@
+"""Keyed-Hash Message Authentication Code Python module
+
+"""
+
+from .khmac import KHMAC
+
+
+# Author info
+__email__ = "yassinmhtat@gmail.com"
+__author__ = "MAHTAT Yassin"
+
+# Project info
+__url__ = "https://github.com/mahtat555/khmac.git"
+__license__ = "MIT"
+__date__ = "2020-01-21"
+__version__ = "0.0.1"
+
+# List of public objects in this package
+__all__ = [
+    'KHMAC'
+]
