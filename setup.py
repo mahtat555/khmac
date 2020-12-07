ef6e8f239db0cb26039ee18cc8598576ece58567
Mahtat <yassinmhtat@gmail.com>
2020-12-07 11:25:27 +0100

    Called the setup() function

--- setup.py
+++ setup.py
@@ -4,7 +4,9 @@ python package in a virtual environment.
 """
 
 from os import path
+from setuptools import setup, find_packages
 
+import khmac
 
 # Current directory
 DIRNAME = path.dirname(__file__)
@@ -31,3 +33,43 @@ def requirements():
     """
     with open(path.join(DIRNAME, "requirements.txt")) as _file:
         return _file.read().splitlines()
+
+
+def main():
+    """Main function
+    """
+    setup(
+        # The name of library
+        name=khmac.__name__,
+        # The code version
+        version=khmac.__version__,
+        # Author info
+        author=khmac.__author__,
+        author_email=khmac.__email__,
+        # An url that points to the official page of your lib
+        url=khmac.__url__,
+        # Licence used
+        license=khmac.__license__,
+        # A short description
+        description="Keyed-Hash Message Authentication Code Python module",
+        # A long description will be displayed to present the lib
+        long_description=readme(),
+        # List the packages to insert in the distribution
+        packages=find_packages(),
+        # A list of strings or a comma-separated string providing
+        # descriptive meta-data
+        keywords="khmac, hmac, mac, python",
+        # Libraries (packages) required for the project to operate
+        install_requires=requirements(),
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved",
+            "Natural Language :: English",
+            "Operating System :: OS Independent",
+        ],
+        python_requires='>=3.6',
+    )
+
+
+if __name__ == "__main__":
+    main()
